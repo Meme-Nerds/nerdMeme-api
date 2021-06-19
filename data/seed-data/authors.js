3b16cfd3cdf3bc3785ac10cdebbfb9fea72f0d54
Jerud-Moyer <jerudmoyer@gmail.com>
2021-06-18 17:39:39 -0700

    initial dev data created

---
+++ data/seed-data/authors.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    world: 'star-wars',
+    data: ['Luke Skywalker', 'Darth Vader', 'Han Solo', 'Princess Leia', 'Ben Kenobi', 'Aunt Veru', 'C-3PO', 'Yoda', '']
+  },
+  {
+    world: 'star-trek',
+    data: ['James T. Kirk', 'Mr. Spock', 'Lt. Uhura', 'Dr. McCoy', 'Mr. Sulu', 'Chekov', 'Scotty', 'Jean luc Picard', 'Lietenant Commander Data']
+  },
+  {
+    world: 'lotr',
+    data: ['Aragorn', 'Galadrielle', 'Bilbo Baggins', 'Gandalf', 'Radagast the Brown', 'Legolas', 'Gimli', 'Arwen', 'Treebeard']
+  },
+  {
+    world: 'harry-potter',
+    data: ['Harry Potter', 'Hermione Granger', 'Dumbledore', 'Professor Snape', 'Voldemort', 'Hagrid', 'Ron Weasley', 'Bathilda Bagshot']
+  },
+  {
+    world: 'marvel',
+    data: ['Tony Stark', 'Peter Parker', 'Natasha Romanoff', 'Thor', 'The Incredible Hulk', 'Dr. Doom', 'Dr. Stephen Strange', 'Thanos']
+  },
+  {
+    world: 'xena',
+    data: ['Xena Warrior Princess', 'Callisto', 'Ares', 'Aphrodite', 'Archangel Michael', 'Caesar']
+  }
+];
---
+++ data/seed-data/settings.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    world: 'star-wars',
+    data: ['a long time ago, in a galaxy far, far away', '...aboard the Deathstar', 'meanwhile on Yavin-4']
+  },
+  {
+    world: 'star-trek',
+    data: ['where no man has gone before', 'Starfleet Academy', 'Planet Triskellion']
+  },
+  {
+    world: 'lotr',
+    data: ['Mordor', 'peeps from Rivendell be like', 'The Shire, 1419']
+  },
+  {
+    world: 'harry-potter',
+    data: ['Hogwarts 1997', 'Malfoy Manor...', 'in the room of requirement']
+  },
+  {
+    world: 'battlestar-galactica',
+    data: ['back on Caprica', 'meanwhile on Scorpia', 'Aboard Battlestar Galactica']
+  },
+  {
+    world: 'marvel',
+    data: ['Stark Tower...', 'just another day on the Quinjet', 'somewhere in Wakanda']
+  },
+  {
+    world: 'xena',
+    data: ['Amphipolis', 'Ancient Athens', 'Corinth, summertime']
+  }
+];
